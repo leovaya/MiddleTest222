e0f0bcfc239dca1d1d4ff1867a45ca1ca686d891
leovaya <lerasmile2066@gmail.com>
2025-05-23 09:42:03 +0300

    add CategoryModelTest

--- gallery/tests.py
+++ gallery/tests.py
@@ -1,3 +1,14 @@
 from django.test import TestCase
+from django.core.files.uploadedfile import SimpleUploadedFile
+from django.core.exceptions import ValidationError
+from gallery.models import Category, Image
+from datetime import date, timedelta
 
-# Create your tests here.
+
+class CategoryModelTest(TestCase):
+
+    def test_create_category(self):
+        category = Category.objects.create(name="Природа")
+        self.assertEqual(category.name, "Природа")
+        self.assertEqual(str(category), "Природа")
+        self.assertEqual(Category.objects.count(), 1)
