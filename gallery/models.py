efc78894577a0c6faf88a817a6005753081ab3d8
leovaya <lerasmile2066@gmail.com>
2025-05-23 09:26:44 +0300

    add Category and Image models

--- gallery/models.py
+++ gallery/models.py
@@ -1,1 +1,25 @@
+
 from django.db import models
+
+# Модель для категорій зображень
+class Category(models.Model):
+    # Назва категорії (наприклад: "Природа", "Міста", "Тварини")
+    name = models.CharField(max_length=100)
+
+    # Метод для зручного відображення об'єкта категорії як рядка
+    def __str__(self):
+        return self.name
+    
+# Модель для зображень
+class Image(models.Model):
+    # Назва зображення
+    title = models.CharField(max_length=200)
+
+    # Поле для зберігання файлу зображення
+    image = models.ImageField(upload_to='images/')
+    categories = models.ManyToManyField(Category, related_name='images')
+    created_date = models.DateField(auto_now_add=True)
+    age_limit = models.PositiveIntegerField() # Вікове обмеження (наприклад: 0, 12, 18 тощо)
+
+    def __str__(self):
+        return self.title
